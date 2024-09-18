f0dddba3312241e5a8a758b19ff7280b5b23a591
Fer <ferp@protonmail.ch>
2024-09-18 14:49:02 +0200

    add basic interaction

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,22 @@ const points: [number, number][] = [
 
 function App() {
   const [dataPoints, setDataPoints] = React.useState(points);
+  const addRandomDatapoint = () => {
+    const x = Math.max(...dataPoints.map((p) => p[0])) + 2;
+    const y = Math.floor(Math.random() * 20) + 1;
+    setDataPoints([...dataPoints, [x, y]]);
+  }
   return (
     <>
       <h1>React + D3js / SVG + Framer Motion</h1>
       <p>
         Interactive animated graphs become simple with this set of technologies
       </p>
+      <div>
+        <button
+        onClick={addRandomDatapoint}
+        >Add Datapoint</button>
+      </div>
       <MotionGraph dataPoints={dataPoints}/>
     </>
   );
